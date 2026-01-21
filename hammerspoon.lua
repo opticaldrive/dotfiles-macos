35b75fe14925762c0428a09c6f32eaef5e9f401a
a <138826219+opticaldrive@users.noreply.github.com>
2026-01-21 00:13:05 -0800

    erm

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -10,6 +10,14 @@ hs.hotkey.bind({"cmd", "alt"}, "B", function()
 end)
 
 
+hs.hotkey.bind({"cmd", "alt"}, "D", function()
+    hs.alert.show("CU printer system enabled rc4")
+    hs.execute("pkill rclone")
+end)
+
+
+
+
 -- Nah - i dont wanna use this since it'll give me annoying popups on the nonadmin.
 -- todo: defaults configure screencapture location, ik it exiss
 -- hs.hotkey.bind({"cmd", "shift"}, "S", function()
